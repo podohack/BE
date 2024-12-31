bfd7025b3dd82557466bbde8ca094e0891a40c82
pipi-shortstocking <jannie435@naver.com>
2024-12-31 18:12:50 +0900

    init

---
+++ src/main/java/com/example/pepperstone/PepperstoneApplication.java
@@ -0,0 +1,13 @@
+package com.example.pepperstone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PepperstoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PepperstoneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pepperstone/PepperstoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pepperstone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PepperstoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
